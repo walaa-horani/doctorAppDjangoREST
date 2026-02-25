44b9dbe44298d50938db32937487547eb6c60615
Your Name <walaa.horani@hotmail.com>
2026-02-25 23:41:19 +0300

    Fix DRF router: add basename to ServiceViewSet

--- backend/apps/api_router.py
+++ backend/apps/api_router.py
@@ -4,7 +4,7 @@ from apps.services.views import ServiceViewSet, AvailabilityViewSet
 from apps.appointments.views import AppointmentViewSet, ReviewViewSet
 
 router = routers.DefaultRouter()
-router.register(r'services', ServiceViewSet)
+router.register(r'services', ServiceViewSet, basename='service')
 router.register(r'availability', AvailabilityViewSet, basename='availability')
 router.register(r'appointments', AppointmentViewSet, basename='appointment')
 router.register(r'reviews', ReviewViewSet)
